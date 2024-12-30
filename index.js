fea7bffa5aca02c3bb9bd04cced6ad26dcf4aef3
Jyotiraditya Bhos <120276415+jyotir2405@users.noreply.github.com>
2024-12-30 14:34:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+import inquirer from 'inquirer';
+import qr from 'qr-image';
+import fs from 'fs';
+
+inquirer
+  .prompt([
+    {"message": "Type your URL: ",
+     "name":"URL"
+    }
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_image.png'));
+    fs.writeFile("input.txt",url, (err)=>{
+        if (err) throw err;
+        console.log("File is saved");
+    });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
